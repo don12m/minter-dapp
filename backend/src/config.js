de9efb29a581c6be96e3caf29f1f88f03010b718
don12m <fasts1616@gmail.com>
2023-01-26 03:49:33 +0100

    neat

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,12 +65,12 @@ const TOKENS_PER_MINT = 60; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2023-01-25T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2023-01-25T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x3ecd60689CCa26C63D56A24b5182575b7c56A195"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0x3ecd60689CCa26C63D56A24b5182575b7c56A195"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
